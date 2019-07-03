b7de1add0e9af65599373815b422e416ff75760d
Clayton Lin <clin.acc.dev@gmail.com>
2019-07-03 17:29:08 -0500

    Stub out commands to run / programs to install

--- install-script.sh
+++ install-script.sh
@@ -2,4 +2,19 @@
 
 echo init main git script
 
+# install chrome
+# install git
+# install vs code
+# install nvm
+# install node using nvm
+# install spotify
+# install slack
+# install evernote
+# install todoist
+# install iterm2
+# install hyper
+# install spectacles
+# install alfred
+# install karabeener
+
 
